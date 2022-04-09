75fdf8feb0613f662f05941a16cdb3b9d46401df
Miles Hoene-Langdon <mileshoenelangdon@Miless-MacBook-Air.local>
2022-04-09 10:50:54 -0500

    changed some things

--- inspect.py
+++ inspect.py
@@ -1,6 +1,6 @@
 def inspect(date_to_find, securities, query_stat):
     # authors: Miles, Nguyen
 
-    prices_df = pd.read_excel("Data/stock_prices.xlsx")
+    prices_df = pd.read_excel("Data/stock_prices.xlsx") # require import pandas as pd in the main program
     
     return prices_df.loc[(prices_df['Date'] == date_to_find) & (prices_df['SecuritiesCode'] == securities), query_stat]
