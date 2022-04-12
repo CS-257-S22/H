2274c0021c3b70709cb902fac1cb245fa568ba18
Jack Huffman <huffmanj@carleton.edu>
2022-04-11 22:04:02 -0500

    4/11/2022 10:00pm

--- DataManipulation.py
+++ DataManipulation.py
@@ -1,6 +1,27 @@
 import csv
+import sys
 import glob
 
-path = "./Data/nasdaq/*.csv"
-for fileName in glob.glob(path):
-    print("Insert code here")
+# combinedFile = "./Data/Combined_Data.csv"
+combinedFile = "./Tests/TestData.csv"
+
+with open(combinedFile, 'w', encoding = "UTF-8", newline = '') as wFile:
+    writer = csv.writer(wFile)
+    # path = "./Data/nasdaq/csv/*.csv"
+    path = "./Data/testfiles/*.csv"
+
+    writer.writerow(['Date', 'Low', 'Open', 'Volume', 'High', 'Close' ,'Adjusted Close', 'Ticker Symbol'])
+    print("Path file is called: " + path)
+    for fileName in glob.glob(path):
+        f = open(fileName, 'r', encoding = "UTF-8")
+        with f as rFile:
+            spamreader = csv.reader(rFile, delimiter=',')
+            print("Reading file: " + fileName)
+            next(spamreader)
+            for item in spamreader:
+                trueFileName = fileName.split()
+                for item in trueFileName:
+                    print(item)
+                # input = item, truestFileName
+                # writer.writerow(input)
+
