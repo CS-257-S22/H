7fa2313e29a919faaa3e33c0acebb889140e96f5
Nguyen Tran <nnt@Nguyens-MacBook-Air.local>
2022-04-16 02:14:30 -0500

    Update main.py to convert Date into Python datetime format

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ from sector_ROI import *
 
 # read all available data
 nasdaq_df = pd.read_csv("Data/Polished/nasdaq_2010_mid_separate_year_month_day.csv")
-
+nasdaq_df["Date"] = pd.to_datetime(nasdaq_df["Date"])
