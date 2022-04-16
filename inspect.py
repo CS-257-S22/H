f11de036fc30c11b5f95a91c0584eda29a57bad5
Nguyen Tran <nnt@Nguyens-MacBook-Air.local>
2022-04-16 15:41:50 -0500

    Reflected changes from Jeff and Jack

--- inspect.py
+++ inspect.py
@@ -1,7 +1,33 @@
+import sys
+import csv
 from winreg import QueryInfoKey
 
+def find_query():
+    num_of_args = len(sys.argv)
+    ticker = sys.argv[0]
+    year = sys.argv[1]
+    month = sys.argv[2]
+    query = sys.argv[3]
+    if not check_num_args(num_of_args):
+        print("There need to be 4 arguments; TickerSymbol, Year, Month, Query")
+        return
 
-def terminal_input_for_basic_stock_stat():
+    if not check_ticker(ticker):
+        print("Ticker not found in dataset")
+        return
+    
+    if not check_year(year):
+        print("Year not in dataset")
+        return
+        
+    if not check_month(month):
+        print("Invalid month")
+        return
+
+    actual_ticker, actual_date, actual_query = inspect_input(num_of_args, ticker, year, month, query)
+    inspect(actual_ticker, actual_date, actual_query, nasdaq_df)
+
+def inspect_input(num_of_args, ticker, year, month, query):
     """
     Author:
         1. Jack
@@ -19,7 +45,46 @@ def terminal_input_for_basic_stock_stat():
         3. query_stat (string, "Open", "Close", "High", etc.)
     """
 
-    return ticker, [date_year, date_month], query_stat # dataformat: string
+    return str(ticker), [str(sys.argv[1]), str(sys.argv[2])], str(sys.argv[3])
+
+    #inspect.py --find_query tickersymbol year month query
+    
+    # return ticker, [date_year, date_month], query_stat # dataformat: string
+
+def check_num_args(num_of_args):
+    if num_of_args != 4:
+        return False
+    return True
+
+def check_ticker(ticker):
+    fileName = "NO_NULL_nasdaq_2010_mid_seperate_year_month_day.csv"
+    f = open(fileName, 'r', encoding = "UTF-8")
+    with f as rFile:
+        spamreader = csv.reader(rFile, delimiter=',')
+        next(spamreader)
+        for row in spamreader:
+            if row[10] == ticker:
+                f.close
+                return True
+    f.close
+    return False 
+    
+    
+def check_year(year):
+    if (2010 <= year <= 2022):
+        return True
+    return False
+
+def check_month(month):
+    if (1 <= month <= 12):
+        return True
+    return False
+    
+def check_query(query):
+    list = ["Low", "Open", "Volume", "High", "Close", "AdjustedClose"]
+    if query in list:
+        return True
+    return False
 
 def inspect(ticker, date, query_stat, dataframe):
      """
