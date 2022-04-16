d7145727606954f7fcc886fca67eab49fd23a7ae
Nguyen Tran <nnt@Nguyens-MacBook-Air.local>
2022-04-16 02:12:41 -0500

    Updated main.py to reflect the new data

--- main.py
+++ main.py
@@ -8,6 +8,5 @@ from inspect import *
 from sector_ROI import *
 
 # read all available data
-prices_df = pd.read_excel("Data/stock_prices.xlsx")
-list_df = pd.read_excel("Data/stock_list_error_free.xlsx")
+nasdaq_df = pd.read_csv("Data/Polished/nasdaq_2010_mid_separate_year_month_day.csv")
 
