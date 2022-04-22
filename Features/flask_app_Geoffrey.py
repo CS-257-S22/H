ea73ca4c9d01575e065eda0c30c6be4ef9342528
Geoffrey Jing <geoffreyjing@Geoffreys-MacBook-Air.local>
2022-04-22 17:15:36 -0500

    Fixed invalid query

--- Features/flask_app_Geoffrey.py
+++ Features/flask_app_Geoffrey.py
@@ -9,6 +9,7 @@ directory = path.Path(__file__).abspath()
 sys.path.append(directory.parent.parent)
 sys.path.append("Features")
 from inspect_stock import find_query, get_fileName
+from basic_stock_stat import get_dates
 
 
 app = Flask(__name__)
@@ -22,8 +23,14 @@ def hello_user():
 def get_query(ticker, year, month, query):
     fileName = get_fileName()
     result = find_query(5, str(ticker), int(year), int(month), str(query), "./Data/Polished/NO_NULL_nasdaq_2010_mid_separate_year_month_day.csv", fileName)
-    # return "The " + str(query) + " price for " + str(ticker) + " during the " + str(month) + "month and " + str(year) + " is " + result
     return "The " + str(query) + " price for " + str(ticker) + " during month " + str(month) + " and year " + str(year) + " is " + str(result)
+
+@app.route('/<ticker>', strict_slashes=False)
+def get_dates_of_stock(ticker):
+    fileName = get_fileName()
+    result = get_dates(str(ticker), fileName)
+    return 
+
 
 
 app.run(host='0.0.0.0', port=81)
