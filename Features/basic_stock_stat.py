b1fdb2720403410c855b90ce89e6640b87faf7b3
Jack Huffman <huffmanj@carleton.edu>
2022-04-25 08:58:59 -0500

    small change

--- Features/basic_stock_stat.py
+++ Features/basic_stock_stat.py
@@ -31,6 +31,9 @@ def get_dates_input():
     fileName = "../Data/Polished/NO_NULL_nasdaq_2010_mid_separate_year_month_day.csv"
     get_dates(ticker, fileName)
 
+def get_nasdaqDates(ticker):
+    fileName = "../Data/Polished/NO_NULL_nasdaq_2010_mid_separate_year_month_day.csv"
+    get_dates(ticker, fileName)
 
 def get_dates(ticker, fileName):
     """
