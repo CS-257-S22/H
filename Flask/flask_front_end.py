37277bcd6770efb9da0ce908a2c20368a3902022
JohnHuffman824 <98602221+JohnHuffman824@users.noreply.github.com>
2022-05-09 22:09:38 -0500

    Update flask_front_end.py

--- Flask/flask_front_end.py
+++ Flask/flask_front_end.py
@@ -29,26 +29,16 @@ def homepage():
 
 #------------------------------
 
-@app.route("/basicData", methods =['GET', 'POST'])
+@app.route("/basicData", methods=['GET', 'POST'])
 def basicData():
     ticker = request.form['ticker']
-    # dates = get_dates(ticker, "./Data/Polished/NO_NULL_nasdaq_2010_mid_separate_year_month_day.csv")
-    dates = getExtremeDates(ticker)
+    dates = get_dates(ticker, "./Data/Polished/NO_NULL_nasdaq_2010_mid_separate_year_month_day.csv")
     max = getMax(ticker)
     min = getMin(ticker)
 
-    # NGUYEN'S CODE
-    # ACCESS THE VARIABLES BELOW TO CALL inspect()
-    # All of them are in integer type
-    earliest_date = dates[0]
-    latest_date = dates[1]
-
-    # EXAMPLE TO GET THE OPEN PRICE OF AAPL ON THE LATEST DATE
-    open_AAPL_earliest = inspect("AAPLE", dates[1], "Open", nasdaq_df)
-
-    earliestData = inspect(ticker, str(dates[0]).split("-"), "Low", nasdaq_df), inspect(ticker, dates[0].split("-"), "High", nasdaq_df), inspect(ticker, dates[0].split("-"), nasdaq_df), inspect(ticker, dates[0].split("-"), "Close", nasdaq_df), inspect(ticker, dates[0].split("-"), "Volume", nasdaq_df)
-    latestData = inspect(ticker, str(dates[1]).split("-"), "Low", nasdaq_df), inspect(ticker, dates[1].split("-"), "High", nasdaq_df), inspect(ticker, dates[1].split("-"), "Open", nasdaq_df), inspect(ticker, dates[1].split("-"), "Close", nasdaq_df), inspect(ticker, dates[1].split("-"), "Volume", nasdaq_df)
-    return render_template("basicData.html", basicTicker=request.args['basicTicker'], earlyDate=dates[0], lateDate=dates[1], max=max[0], maxDay=max[1], min=min[0], minDay=min[1], earlyLow=earliestData[0], earlyHigh=earliestData[1], earlyOpen=earliestData[2], earlyClose=earliestData[3], earlyVolume=earliestData[4], lateLow=latestData[0], lateHigh=latestData[1], lateOpen=latestData[2], lateClose=latestData[3], lateVolume=latestData[4])
+    earliestData = inspect(ticker, dates[0], "Low", nasdaq_df), inspect(ticker, dates[0], "High", nasdaq_df), inspect(ticker, dates[0], "Open", nasdaq_df), inspect(ticker, dates[0], "Close", nasdaq_df), inspect(ticker, dates[0], "Volume", nasdaq_df)
+    latestData = inspect(ticker, dates[1], "Low", nasdaq_df), inspect(ticker, dates[1], "High", nasdaq_df), inspect(ticker, dates[1], "Open", nasdaq_df), inspect(ticker, dates[1], "Close", nasdaq_df), inspect(ticker, dates[1], "Volume", nasdaq_df)
+    return render_template("basicData.html", basicTicker=ticker, earlyDate=dates[0], lateDate=dates[1], max=max[0], maxDay=max[1], min=min[0], minDay=min[1], earlyLow=earliestData[0], earlyHigh=earliestData[1], earlyOpen=earliestData[2], earlyClose=earliestData[3], earlyVolume=earliestData[4], lateLow=latestData[0], lateHigh=latestData[1], lateOpen=latestData[2], lateClose=latestData[3], lateVolume=latestData[4])
 
 #------------------------------
 
