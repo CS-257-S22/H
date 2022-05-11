771874924940a68da0a1a9e6ca52b42f9bce3d29
Nguyen Tran <nguyen.tran.2002.work@gmail.com>
2022-05-11 10:43:07 -0500

    New test for new changes

---
+++ Tests/test_querry_edge.py
@@ -0,0 +1,24 @@
+# Pycache are evil, don't produce them
+import sys
+sys.dont_write_bytecode = True
+
+import sys
+sys.path.append('../H/Features')
+import pandas as pd
+import unittest
+from inspect_stock import *
+
+class TestInspectCase(unittest.TestCase):
+    def test_case_(self):
+        """
+        Tests that error message is output when an invalid data is input, for the first feature inspect_stock that returns 
+        the specified price of a stock from inputted ticker symbol, date, and query. 
+        """
+        nasdaq_df = pd.read_csv("Data/Polished/randomized_day_market.csv")
+        output = find_query(6, "AAPL", 2009, 9, "Volume", "Data/Polished/randomized_day_market.csv", nasdaq_df)
+        error_message = "Invalid Date"
+        self.assertEqual(output, error_message)
+        print(output)
+
+if __name__ == '__main__':
+    unittest.main()
