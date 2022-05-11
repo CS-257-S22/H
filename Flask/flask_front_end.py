e3feb3c9454f984e4c5fe786bd28a24e4cb6941f
Nguyen Tran <nguyen.tran.2002.work@gmail.com>
2022-05-10 21:51:45 -0500

    Added stock_ROI method to the flask

--- Flask/flask_front_end.py
+++ Flask/flask_front_end.py
@@ -2,9 +2,18 @@
 import sys
 sys.dont_write_bytecode = True
 
-from urllib import request
-from flask import render_template, Flask, request
+from flask import render_template, Flask, request, url_for
+
 import pandas as pd
+import seaborn as sns
+# set desired graph size
+sns.set(rc={'figure.figsize':(15,10)})
+# set background color
+sns.set(rc={"axes.facecolor":"white", "figure.facecolor":"white"})
+
+# attempting to fix NSWindow Drag Exception
+import matplotlib
+matplotlib.pyplot.switch_backend('Agg')
 
 import sys
 sys.path.append('Features')
@@ -25,7 +34,7 @@ nasdaq_df["Date"] = pd.to_datetime(nasdaq_df["Date"])
 
 @app.route("/")
 def homepage():
-    return render_template('index_mainpage.html', tickers=all_tickers())
+    return render_template('index_mainpage.html', tickers = all_tickers())
 
 #------------------------------
 
@@ -42,9 +51,31 @@ def basicData():
 
 #------------------------------
 
-@app.route("/stockROI", methods=['GET', 'POST'])
+@app.route("/stock_ROI")
 def stock_ROI():
-    return request.form['ticker']
+    """
+    DESCRIPTION:
+        This is the back-end of the stock_ROI page.
+
+    INPUT SIGNATURE:
+        1. ticker (string): the user only need to input a ticker
+
+    OUTPUT SIGNATURE:
+        1. graph: generate a graph visualizing the stock's ROI overtime
+    """
+
+    # ticker of the requested stock
+    ticker = request.args["ticker"]
+
+    # generate the requested ticker's yearly ROI graph in the back-end
+    graph = graph_ROIs_over_time_one_stock(ticker, nasdaq_df)
+
+    # get the proper path
+    graph_url = url_for('static', filename = "photos/graphs/" + graph)
+    # graph_location = "../../static/photos/" + graph
+
+    # display the webpage that contains the graph
+    return render_template("stock_ROI_graph.html", ticker = ticker, graph_image = graph_url)
 
 if __name__ == '__main__':
      app.run()
