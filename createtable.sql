46732fe894ad70a64a80dda65478f9f13cd8e539
Nguyen Tran <nnt@Nguyens-MacBook-Air.local>
2022-05-13 06:15:15 -0500

    added sql script for team database

---
+++ createtable.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS nasdaq;
+CREATE TABLE nasdaq (
+  Recorded_Date date,
+  Recorded_Day real,
+  Recorded_Month real,
+  Recorded_Year real,
+  Low real,
+  Recorded_Open real,
+  Volume real,
+  High real,
+  Recorded_Close real,
+  Ticker_Symbol text
+);
