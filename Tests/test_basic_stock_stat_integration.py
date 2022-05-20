cd2e8b1fcd2460a19c0fda22afde89c5cee62d5e
Miles Hoene-Langdon <mileshoenelangdon@Miless-MacBook-Air.local>
2022-05-19 20:19:21 -0500

    fixed basic stock integration test

--- Tests/test_basic_stock_stat_integration.py
+++ Tests/test_basic_stock_stat_integration.py
@@ -17,7 +17,7 @@ class TestInspectCase(unittest.TestCase):
 
         test_dates_sample_df = pd.read_csv("Tests/DataForTesting/test_date_data_sample.csv")
 
-        output_get_dates = get_dates("IIN", 'Tests/DataForTesting/test_date_data_sample.csv')
+        output_get_dates = get_dates("IIN")
         output_find_extreme_dates = (find_earliest_or_latest_record("IIN", "earliest",test_dates_sample_df), find_earliest_or_latest_record("IIN", "latest",test_dates_sample_df))
         self.assertEqual(output_get_dates, output_find_extreme_dates)
 
