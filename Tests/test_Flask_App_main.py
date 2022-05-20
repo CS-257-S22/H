0bd1f35acabe7a66d439ec522b1ecabf1f65dd80
Nguyen Tran <nnt@Nguyens-MacBook-Air.local>
2022-05-20 10:11:35 -0500

    bruh

--- Tests/test_Flask_App_main.py
+++ Tests/test_Flask_App_main.py
@@ -80,7 +80,7 @@ class test_Flask_app(unittest.TestCase):
         routes = route_generator_stock_ROI(given)
 
         # list of corresponding expected output
-        expected = [49.705884974537206, 115.72103921641789]
+        expected = [49.705882352941174, 115.72103921641789]
 
         # loop through all the possible routes/input and compare the result
         for i in range(len(routes)):
