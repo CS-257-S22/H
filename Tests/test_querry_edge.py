69b08292ede1370ed72af9d185a936424dd28987
Miles Hoene-Langdon <mileshoenelangdon@Miless-MacBook-Air.local>
2022-05-19 22:06:49 -0500

    fixed query unit test

--- Tests/test_querry_edge.py
+++ Tests/test_querry_edge.py
@@ -7,6 +7,7 @@ sys.path.append('../H/Features')
 import pandas as pd
 import unittest
 from inspect_stock import *
+from helper import *
 
 class TestInspectCase(unittest.TestCase):
     def test_case_(self):
@@ -14,8 +15,9 @@ class TestInspectCase(unittest.TestCase):
         Tests that error message is output when an invalid data is input, for the first feature inspect_stock that returns 
         the specified price of a stock from inputted ticker symbol, date, and query. 
         """
-        nasdaq_df = pd.read_csv("Data/Polished/randomized_day_market.csv")
-        output = find_query(6, "AAPL", 2009, 9, "Volume", "Data/Polished/randomized_day_market.csv", nasdaq_df)
+        nasdaq_df = get_dataframe()
+        
+        output = find_query(6, "AAPL", 2009, 9, "Volume", nasdaq_df)
         error_message = "Invalid Date"
         self.assertEqual(output, error_message)
         print(output)
