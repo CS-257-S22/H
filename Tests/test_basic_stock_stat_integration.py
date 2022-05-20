e8cd9cee7b370666824aba6c4c04d4cc4cebd9e0
Miles Hoene-Langdon <mileshoenelangdon@Miless-MacBook-Air.local>
2022-05-19 20:44:12 -0500

    fix basic stock stat test

--- Tests/test_basic_stock_stat_integration.py
+++ Tests/test_basic_stock_stat_integration.py
@@ -1,11 +1,14 @@
 # Pycache are evil, don't produce them
 import sys
+
+from Features.helper import get_dataframe
 sys.dont_write_bytecode = True
 
 import unittest
 import sys
 sys.path.append('../H/Features')
 from basic_stock_stat import *
+from helper import *
 import pandas as pd
 
 class TestInspectCase(unittest.TestCase):
@@ -15,10 +18,10 @@ class TestInspectCase(unittest.TestCase):
         find_earlist_or_latest_record
         """
 
-        test_dates_sample_df = pd.read_csv("Tests/DataForTesting/test_date_data_sample.csv")
+        nasdaq = get_dataframe()
 
         output_get_dates = get_dates("IIN")
-        output_find_extreme_dates = (find_earliest_or_latest_record("IIN", "earliest",test_dates_sample_df), find_earliest_or_latest_record("IIN", "latest",test_dates_sample_df))
+        output_find_extreme_dates = (find_earliest_or_latest_record("IIN", "earliest", nasdaq), find_earliest_or_latest_record("IIN", "latest", nasdaq))
         self.assertEqual(output_get_dates, output_find_extreme_dates)
 
 if __name__ == '__main__':
