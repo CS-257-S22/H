274d49facc8d374a7e2da57e90aef6a3d55d7e05
Nguyen Tran <nnt@Nguyens-MacBook-Air.local>
2022-05-26 21:20:41 -0500

    universal import

---
+++ Features/universal_import.py
@@ -0,0 +1,30 @@
+# STOP PYCACHE
+import fileinput
+import sys
+sys.dont_write_bytecode = True
+
+#------------------------------
+# LIBRARIES
+
+import path
+import argparse
+import csv
+from fileinput import filename
+
+import pandas as pd
+import matplotlib
+import datetime
+
+import seaborn as sns
+# set desired graph size
+sns.set(rc={'figure.figsize':(10,5)})
+# set background color
+sns.set(rc={"axes.facecolor":"white", "figure.facecolor":"white"})
+
+from enum import unique
+from logging import raiseExceptions
+
+#------------------------------
+# IMPORT FEATURES
+
+# We do NOT import all features in the universal import file because we don't want a feature to call itself.
