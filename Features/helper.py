e2ca9e32bd7af6277e4ecb910f0b466e42b54206
Geoffrey Jing <geoffreyjing@Geoffreys-MacBook-Air.local>
2022-05-28 01:33:29 -0500

    merge

--- Features/helper.py
+++ Features/helper.py
@@ -55,7 +55,7 @@ def get_max(inputTicker):
     if not check_ticker(inputTicker):
         return "Please input a valid ticker symbol"
 
-    cursor.execute("SELECT MAX(high), rec_date FROM nasdaq WHERE ticker=inputTicker GROUP BY rec_date;", inputTicker)
+    cursor.execute("SELECT MAX(high), rec_date FROM nasdaq WHERE ticker=inputTicker GROUP BY rec_date;")
     table = cursor.fetchall()
     return table[0][0], table[0][1]
 
@@ -70,7 +70,7 @@ def get_min(inputTicker):
     if not check_ticker(inputTicker):
         return "Please input a valid ticker symbol"
 
-    cursor.execute("SELECT MIN(low), rec_date FROM nasdaq WHERE ticker=inputTicker GROUP BY rec_date;", inputTicker)
+    cursor.execute("SELECT MIN(low), rec_date FROM nasdaq WHERE ticker=inputTicker GROUP BY rec_date;")
     table = cursor.fetchall()
     return table[0][0], table[0][1]
 
